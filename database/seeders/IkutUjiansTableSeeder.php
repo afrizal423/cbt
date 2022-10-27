8e577798f26fc9a064a69fc2a798ccd7b69516a0
afrizal423 <afrizal2499@gmail.com>
2022-10-27 08:38:19 +0700

    wip starter ujian

--- database/seeders/IkutUjiansTableSeeder.php
+++ database/seeders/IkutUjiansTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Siswa;
+use App\Models\Ujian;
 use Illuminate\Database\Seeder;
 
 class IkutUjiansTableSeeder extends Seeder
@@ -14,21 +16,31 @@ class IkutUjiansTableSeeder extends Seeder
      */
     public function run()
     {
-        
 
+        $siswa = Siswa::all();
+        $ujian = Ujian::all();
         \DB::table('ikut_ujians')->delete();
-        
-        \DB::table('ikut_ujians')->insert(array (
-            0 => 
-            array (
-                'id' => '01ge17tvy7xq2dv1jr5ercxr34',
-                'siswa_id' => '01ge17pg494yx1s29qycjsjvxp',
-                'ujian_id' => '01ge17rk63nfzk87z1sd4fm5xd',
-                'status' => true,
-                'sudah_ujian' => false,
-            ),
-        ));
-        
-        
+
+        foreach ($ujian as $key1 => $u) {
+            foreach ($siswa as $key => $sswa) {
+                if ($u->kelas_id == $sswa->kelas_id) {
+                    \DB::table('ikut_ujians')->insert(array (
+                        'id' => '01ge17tvy7xq2dv1jr5ercxr34',
+                        'siswa_id' => $sswa->id,
+                        'ujian_id' => $ujian->id,
+                        'status' => true,
+                        'sudah_ujian' => false,
+                ));
+                }
+            }
+        }
+
+
+
+
+
+
+
+
     }
 }
