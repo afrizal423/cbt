d0c2c9c0f813267dbcb9899ef0b2ddd12568a3ca
afrizal423 <afrizal2499@gmail.com>
2022-10-27 14:08:33 +0700

    issue #4 task no 2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\ketikaWaktuUjianHabis;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +17,16 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        /**
+         * Dokumentasi scheduler
+         * https://laravel.com/docs/9.x/scheduling#schedule-frequency-options
+         */
+        $schedule->job(new ketikaWaktuUjianHabis())
+                    ->weekdays()
+                    ->everyFiveMinutes()
+                    ->timezone('Asia/Jakarta')
+                    ->between('6:00', '21:00');
     }
 
     /**
---
+++ app/Jobs/ketikaWaktuUjianHabis.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Nilai;
+use App\Models\Ujian;
+use App\Models\IkutUjian;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class ketikaWaktuUjianHabis implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $u = Ujian::with(['nilais'])->where('status_jobs_selesai_ujian', false)->get();
+
+        foreach ($u as $key => $ujn) {
+            $sekarang = Carbon::now();
+            $mulai = Carbon::parse($ujn->tgl_selesai_ujian.' '.$ujn->waktu_selesai_ujian);
+            $waktuMulaiUjian = $sekarang->gt($mulai);
+            $nilai = Nilai::where('ujian_id', $ujn->id)->get();
+            foreach ($nilai as $key => $value) {
+                if ($value->status_penilaian == null && $waktuMulaiUjian) {
+                    Nilai::where('ujian_id', $ujn->id)
+                        ->where('siswa_id', $value->siswa_id)
+                        ->update([
+                            'nilai_ujian' => 0,
+                            'status_penilaian' => false
+                        ]);
+                    IkutUjian::updateOrCreate([
+                        'siswa_id' => $value->siswa_id,
+                        'ujian_id' => $ujn->id
+                    ],[
+                        'sudah_ujian' => true
+                    ]);
+                }
+            }
+        }
+    }
+}
