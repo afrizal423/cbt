26ab110a4b95294d29ac1074a9d3edd84633a8b3
afrizal423 <afrizal2499@gmail.com>
2022-11-04 09:48:13 +0700

    done gate ujian dan end ujian
    ketika siswa sudah ujian tidak bisa akses playground ujian.
    tombol menghentikan ujian (selesai)

--- app/Http/Livewire/Siswa/UjianPlayground.php
+++ app/Http/Livewire/Siswa/UjianPlayground.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Livewire\Siswa;
 
-use App\Models\JawabanUjian;
-use App\Models\ListJawabansoal;
 use App\Models\Ujian;
 use Livewire\Component;
+use App\Models\IkutUjian;
+use App\Models\JawabanUjian;
+use App\Models\ListJawabansoal;
+use App\Jobs\penilaianUjianSiswa;
 use App\Models\SoalnyaSiswaUjian;
 use Illuminate\Support\Facades\Auth;
 
@@ -70,44 +72,33 @@ class UjianPlayground extends Component
     public function tombolHentikanUjian()
     {
         // dd($this->soal);
-        JawabanUjian::updateOrCreate([
-            'siswa_id' => Auth::guard('siswa')->user()->id,
-            'ujian_id' => $this->ujian_id,
-            'soal_id' => $this->soal_id],
-            [
-                'jawaban_siswa' => isset($this->jawaban['siswa']) ? json_encode($this->jawaban['siswa']) : null,
-                'ragu_jawaban' => isset($this->jawaban['ragu-ragu']) ? $this->jawaban['ragu-ragu'] : false
-            ]
-        );
-
-        $this->siswaRagu = JawabanUjian::where('siswa_id', Auth::guard('siswa')->user()->id)
-                                ->where('ujian_id', $this->ujian_id)
-                                ->get()
-                                ->toArray();
-        $this->soal = Ujian::select('mapel_id','tgl_selesai_ujian','waktu_selesai_ujian')->with([
-            'mapel' => function($q){
-                $q->select('id');
-                $q->with(
-                    [
-                        'soals' => function($q){
-                            $siswa = Auth::guard('siswa')->user();
-                            $ambilIdSoal = SoalnyaSiswaUjian::where('siswa_id', $siswa->id)
-                                    ->where('ujian_id', $this->ujian_id)->first();
-                            $this->listsoal = json_decode($ambilIdSoal->listsoal);
-                            $q->select('id', 'mapel_id', 'soal', 'type_soal', 'bobot_soal');
-                            $q->where('id', $this->listsoal[$this->nomor_soal-1]);
-                            $q->first();
-                        }
-                    ]
-                );
-            }
-        ])->where('id',$this->ujian_id)->first();
+        $this->mount();
         $this->dispatchBrowserEvent('openModal');
     }
 
     public function hentikanUjian()
     {
-        dd(true);
+        $userId= Auth::guard('siswa')->user()->id;
+        $jumlahRagu = JawabanUjian::where('ujian_id', $this->ujian_id)
+                            ->where('siswa_id', $userId)
+                            ->where('ragu_jawaban', true)
+                            ->count();
+        if ($jumlahRagu != 0) {
+            $this->mount();
+            $this->dispatchBrowserEvent('masihAdaRagu');
+        } else {
+            $this->mount();
+            $dt['siswa_id'] = $userId;
+            $dt['ujian_id'] = $this->ujian_id;
+            penilaianUjianSiswa::dispatch($dt);
+            IkutUjian::updateOrCreate([
+                'siswa_id' => $userId,
+                'ujian_id' => $this->ujian_id
+            ],[
+                'sudah_ujian' => true
+            ]);
+            return redirect()->route('siswa.dashboard');
+        }
     }
     public function mount(){
         $this->soal = Ujian::select('mapel_id','tgl_selesai_ujian','waktu_selesai_ujian')->with([
