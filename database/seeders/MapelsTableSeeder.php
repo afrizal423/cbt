263a23b30e70701e81f5ba8465483e1daa65c6da
afrizal423 <afrizal2499@gmail.com>
2022-12-13 02:08:50 +0000

    seeder new mapel

--- database/seeders/MapelsTableSeeder.php
+++ database/seeders/MapelsTableSeeder.php
@@ -30,6 +30,17 @@ class MapelsTableSeeder extends Seeder
                 'jumlah_essai' => 5,
                 'status_mapel' => NULL,
             ),
+            1 => 
+            array (
+                'id' => '01gkv0cavf1hhcyd8s0hg1552d',
+                'kode_mapel' => 'PPKN6',
+                'nama_mapel' => 'UH PPKN Kelas 6',
+                'kkm_mapel' => '70',
+                'jumlah_opsi_jawaban' => 0,
+                'jumlah_pilihan_ganda' => 0,
+                'jumlah_essai' => 15,
+                'status_mapel' => true,
+            ),
         ));
         
         
